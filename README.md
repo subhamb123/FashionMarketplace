e067e692d275d2226a5c15625c2b8df3f05ef93d
Daniel <daniel.e.ochoa@wsu.edu>
2024-04-06 18:04:08 -0700

    reset

--- README.md
+++
@@ -1,3 +0,0 @@
-# FashionMarketplace [![license](https://img.shields.io/github/license/DAVFoundation/captain-n3m0.svg?style=flat-square)](https://github.com/subhamb123/TotallyReddit/blob/main/LICENSE)
-
-we sell good clothes
--- validate.js
+++
@@ -1,9 +0,0 @@
-function validateEmail() {
-    const email = document.getElementById("email").value;
-    //console.log(email);
-    const valid = email.includes("@") && email.endsWith(".com");
-    if (!valid) {
-      alert("invalid email!");
-    }
-    return valid;
-  }
